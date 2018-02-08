3db88fc87b9dfce7cee0a25dd81754edd61e53b0
Toby Bro <thetobiasbrodersen@gmail.com>
2018-02-08 13:25:24 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,9 +12,9 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board, index)
-  if board[index] == " "
-    return true
-  else
+  if board[index] != " "
     return false
+  else
+    return true
   end
 end
